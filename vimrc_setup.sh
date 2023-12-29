cb5b5a9019ba5f78c3b3660a360993c9b1c3e45e
horus <h.horace0921@gmail.com>
2023-12-29 21:08:35 +0800

    feat: NerdTree setup

--- vimrc_setup.sh
+++ vimrc_setup.sh
@@ -8,4 +8,8 @@ fi
 
 curl https://raw.githubusercontent.com/nubiv/vimrc/main/.vimrc >> ~/.vimrc
 
+# Set up NerdTree
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+
 echo "Completed Vim configuration successfully! Enjoy :-)"
