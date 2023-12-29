5ff9abe36250a1eb9e9c5f7604dadd1cc6cfeed0
horus <h.horace0921@gmail.com>
2023-12-29 21:12:37 +0800

    chore: add feedback on NerdTree setup completion

--- vimrc_setup.sh
+++ vimrc_setup.sh
@@ -11,5 +11,6 @@ curl https://raw.githubusercontent.com/nubiv/vimrc/main/.vimrc >> ~/.vimrc
 # Set up NerdTree
 git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
 vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+echo "NerdTree setup done."
 
 echo "Completed Vim configuration successfully! Enjoy :-)"
